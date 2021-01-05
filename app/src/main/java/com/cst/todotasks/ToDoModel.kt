875370fb2ff4a168cee02c2b167b02b7fe22d2a5
Ilia Guchashvili <Werutgbs@gmail.com>
2021-01-06 00:32:14 +0400

    First Commit

--- app/src/main/java/com/cst/todotasks/ToDoModel.kt
+++ app/src/main/java/com/cst/todotasks/ToDoModel.kt
@@ -9,7 +9,7 @@ import java.util.*
 @Entity
 data class ToDoModel(
     @PrimaryKey(autoGenerate = true) val uid: Int,
-    @ColumnInfo(name = "title") val title: String?,
-    @ColumnInfo(name = "text") val text: String?,
+    @ColumnInfo(name = "title") var title: String?,
+    @ColumnInfo(name = "text") var text: String?,
     @ColumnInfo(name = "isDone") var isDone: Boolean?=false
 )
--- app/src/main/java/com/cst/todotasks/ui/EditTask.kt
+++ app/src/main/java/com/cst/todotasks/ui/EditTask.kt
@@ -1,13 +1,18 @@
 package com.cst.todotasks.ui
 
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 import com.cst.todotasks.DBInstance.Companion.toDoDB
 import com.cst.todotasks.R
+import com.cst.todotasks.ToDoModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -15,6 +20,9 @@ import kotlinx.coroutines.withContext
 
 
 class EditTask() : Fragment() {
+    lateinit var task: ToDoModel
+    lateinit var text: EditText
+    lateinit var title: EditText
 
 
     override fun onCreateView(
@@ -27,11 +35,29 @@ class EditTask() : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val uid =  arguments!!.getInt("uid")
+        val uid = arguments!!.getInt("uid")
         GlobalScope.launch {
             withContext(Dispatchers.IO) {
-                toDoDB.userDao().selectById(uid)
+                task = toDoDB.userDao().selectById(uid)
+                withContext(Dispatchers.Main) {
+                    text = activity!!.findViewById<EditText>(R.id.textText)
+                    title = activity!!.findViewById<EditText>(R.id.titleText)
+                    text.text = Editable.Factory.getInstance().newEditable(task.text)
+                    title.text = Editable.Factory.getInstance().newEditable(task.title)
+                }
+
             }
+        }
+        val submitButton = activity!!.findViewById<ImageButton>(R.id.submitButton)
+        submitButton.setOnClickListener {
+            task.title = title.text.toString()
+            task.text = text.text.toString()
+            GlobalScope.launch {
+                withContext(Dispatchers.IO) {
+                    toDoDB.userDao().updateTask(task)
+                }
+            }
+            activity!!.supportFragmentManager.popBackStack();
         }
 
     }
